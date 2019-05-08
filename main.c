bb301bfb106f77801b19991f9b75cb2ebd6cd518
Hales19 <a.sarvetnikov@mail.ru>
2019-05-08 01:23:13 -0400

    Add main.c

--- main.c
+++ main.c
@@ -31,7 +31,7 @@ int main()
     int arr[] = {3, 1, 4, 1, 5, 9, 2};
 
     const int min = min_element(arr, N_ELEMENTS(arr));
-
+//666
     print_array(arr, N_ELEMENTS(arr));
     printf("Min element: %d\n", min);
 
